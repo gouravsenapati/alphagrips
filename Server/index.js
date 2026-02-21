c959b41085bd829b9dc1e0b86f40fa401e702318
gouravsenapati <gourav.giftian@gmail.com>
2026-02-21 15:02:09 +0500

    Add request logging and update server response

--- Server/index.js
+++ Server/index.js
@@ -2,16 +2,23 @@ import express from "express";
 
 const app = express();
 
+app.use((req,res,next)=>{
+  console.log("REQUEST:", req.method, req.url);
+  next();
+});
+
 app.get("/", (req, res) => {
-  res.send("AlphaGrips API Running");
+  res.send("SERVER WORKING");
 });
 
 app.get("/health", (req, res) => {
   res.send("OK");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+
+console.log("PORT ENV =", PORT);
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port", PORT);
+  console.log("Listening on", PORT);
 });
