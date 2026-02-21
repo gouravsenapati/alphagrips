39663fcaabc07a834cdebd1e4e94e93f74c2c587
gouravsenapati <gourav.giftian@gmail.com>
2026-02-21 13:55:22 +0500

    updated port 8080
    updated port section

--- Server/index.js
+++ Server/index.js
@@ -1001,7 +1001,8 @@ app.get("/", (req, res) => {
 /* ======================
    START SERVER
 ====================== */
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
