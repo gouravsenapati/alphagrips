c27f0a8beba0c5b0110a4b5f4d862d9a50e0ce62
gouravsenapati <gourav.giftian@gmail.com>
2026-02-21 14:04:06 +0500

    debugging2

--- Server/index.js
+++ Server/index.js
@@ -19,6 +19,18 @@ console.log("RAZORPAY_KEY_SECRET:", process.env.RAZORPAY_KEY_SECRET ? "OK" : "MI
 const app = express();
 app.use(cors());
 app.use(express.json());
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve frontend
+app.use(express.static(path.join(__dirname, "../Client")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Client/index.html"));
+});
+
 app.get("/", (req, res) => {
   res.send("AlphaGrips API Running");
 });
@@ -1017,5 +1029,6 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 
