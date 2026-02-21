e2f5da07b50e38624b9fc0da0febd88132e1adfd
gouravsenapati <gourav.giftian@gmail.com>
2026-02-21 15:00:50 +0500

    Change server listen address to '0.0.0.0'

--- Server/index.js
+++ Server/index.js
@@ -12,6 +12,6 @@ app.get("/health", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port", PORT);
 });
